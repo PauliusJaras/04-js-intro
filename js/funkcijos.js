f6e482c707727a22d37d20c0c1e10513f788ec39
pauliusjaras <paulius.jaras@outlook.com>
2022-12-20 12:21:31 +0200

    didziausiasSkaiciusSarase update

--- js/funkcijos.js
+++ js/funkcijos.js
@@ -43,15 +43,16 @@ console.groupEnd();
 console.groupCollapsed("#4: Funkcija pavadinimu “didziausiasSkaiciusSarase”:")
 
 function didziausiasSkaiciusSarase(arr){
-    if(typeof arr != typeof []){
+    if(!Array.isArray(arr)){
         return "Pateikta netinkamo tipo reikšmė.";
     } else if(arr.length == 0){
         return "Pateiktas sąrašas negali būti tuščias.";
     } else {
-        let highestNumber = 0;
+        let highestNumber = [-1];
         for(ar of arr){
-            highestNumber == 0 ? (highestNumber += ar) : 0;
-            ar > highestNumber ? (highestNumber = ar) : 0; 
+            if(!isNaN(ar) && isFinite(ar) && typeof ar != typeof true && typeof ar != typeof []){
+                highestNumber < ar ? (highestNumber = ar) : 0;
+            } 
         }
         return highestNumber;
     }
@@ -64,57 +65,81 @@ console.log(didziausiasSkaiciusSarase([ 69, 69, 69, 69, 66 ]));
 console.log(didziausiasSkaiciusSarase([ -1, -2, -3, -4, -5, -6, -7, -8 ]));
 console.log(didziausiasSkaiciusSarase("pomidoras"));
 console.log(didziausiasSkaiciusSarase([]));
-console.groupEnd();
 
-console.groupCollapsed("#5: Funkcija pavadinimu “isrinktiRaides”:")
+console.log(didziausiasSkaiciusSarase([1,2,3, Infinity]));
+console.log(didziausiasSkaiciusSarase([1,2,Infinity, 3]));
+console.log(didziausiasSkaiciusSarase([1,Infinity,2,3]));
+console.log(didziausiasSkaiciusSarase([Infinity, 1,2,3]));
 
-function isrinktiRaides(text, num){
-    if(typeof text != 'string'){
-        return "Pirmasis kintamasis yra netinkamo tipo";
-    } else if(text.length <= 0 || text.length >= 100) {
-            return "Pirmojo kintamojo reikšmė yra netinkamo dydžio.";
-    } else if(typeof num != typeof 1 || Number.isNaN(num) == true){
-            return "Antrasis kintamasis yra netinkamo tipo.";
-    } else if(num <= 0){
-        return "Antrasis kintamasis turi būti didesnis už nulį.";
-    } else if (text.length < num){
-        return "Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį";
-    } else{
-        let result = "";
-        for(let i = num-1; i < text.length; i+=num ){
-            result += text[i];
-        }
-        return result;
-    }
-}
+console.log(didziausiasSkaiciusSarase([1,2,3, NaN]));
+console.log(didziausiasSkaiciusSarase([1,2,NaN, 3]));
+console.log(didziausiasSkaiciusSarase([1,NaN,2,3]));
+console.log(didziausiasSkaiciusSarase([NaN, 1,2,3]));
 
-console.log(isrinktiRaides("abcdefg", 2));
-console.log(isrinktiRaides("abcdefghijkl", 3));
-console.log(isrinktiRaides("abc", 0));
-console.log(isrinktiRaides("abc", 4));
-console.log(isrinktiRaides(1561, 2));
-console.groupEnd();
+console.log(didziausiasSkaiciusSarase([1,2,3, true]));
+console.log(didziausiasSkaiciusSarase([1,2,true, 3]));
+console.log(didziausiasSkaiciusSarase([1,true,2,3]));
+console.log(didziausiasSkaiciusSarase([true, 0,-1,-2]));
 
-console.groupCollapsed("#6: Funkcija pavadinimu “dalyba”:")
+console.log(didziausiasSkaiciusSarase([1,2,3, 'true']));
+console.log(didziausiasSkaiciusSarase([1,2,'true', 3]));
+console.log(didziausiasSkaiciusSarase([1,'true',2,3]));
+console.log(didziausiasSkaiciusSarase(['true', 1,2,3]));
 
-function dalyba(a,b){
-    if(typeof a != typeof 1 || Number.isNaN(a) == true){
-        return "Pirmasis kintamasis yra netinkamo tipo";
-    } else if(typeof b != typeof 1 || Number.isNaN(b) == true){
-        return "Antrasis kintamasis yra netinkamo tipo.";
-    } else if(b == 0){
-        return "Dalyba is nulio negalima";
-    } else {
-        let result = a / b;
-        return result;
-    }
-}
-
-console.log(dalyba(4,2));
-console.log(dalyba("7a",2));
-console.log(dalyba(4,"2"));
-console.log(dalyba(6,0));
-console.log(dalyba(0,9));
-console.groupEnd();
+console.log(didziausiasSkaiciusSarase(['true', true, NaN, [], [5], Infinity, -Infinity, 0]));
+console.log(didziausiasSkaiciusSarase(['true', true, NaN, [], [5], Infinity, -Infinity]));
 
 console.groupEnd();
+
+// console.groupCollapsed("#5: Funkcija pavadinimu “isrinktiRaides”:")
+
+// function isrinktiRaides(text, num){
+//     if(typeof text != 'string'){
+//         return "Pirmasis kintamasis yra netinkamo tipo";
+//     } else if(text.length <= 0 || text.length >= 100) {
+//             return "Pirmojo kintamojo reikšmė yra netinkamo dydžio.";
+//     } else if(typeof num != typeof 1 || Number.isNaN(num) == true){
+//             return "Antrasis kintamasis yra netinkamo tipo.";
+//     } else if(num <= 0){
+//         return "Antrasis kintamasis turi būti didesnis už nulį.";
+//     } else if (text.length < num){
+//         return "Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį";
+//     } else{
+//         let result = "";
+//         for(let i = num-1; i < text.length; i+=num ){
+//             result += text[i];
+//         }
+//         return result;
+//     }
+// }
+
+// console.log(isrinktiRaides("abcdefg", 2));
+// console.log(isrinktiRaides("abcdefghijkl", 3));
+// console.log(isrinktiRaides("abc", 0));
+// console.log(isrinktiRaides("abc", 4));
+// console.log(isrinktiRaides(1561, 2));
+// console.groupEnd();
+
+// console.groupCollapsed("#6: Funkcija pavadinimu “dalyba”:")
+
+// function dalyba(a,b){
+//     if(typeof a != typeof 1 || Number.isNaN(a) == true){
+//         return "Pirmasis kintamasis yra netinkamo tipo";
+//     } else if(typeof b != typeof 1 || Number.isNaN(b) == true){
+//         return "Antrasis kintamasis yra netinkamo tipo.";
+//     } else if(b == 0){
+//         return "Dalyba is nulio negalima";
+//     } else {
+//         let result = a / b;
+//         return result;
+//     }
+// }
+
+// console.log(dalyba(4,2));
+// console.log(dalyba("7a",2));
+// console.log(dalyba(4,"2"));
+// console.log(dalyba(6,0));
+// console.log(dalyba(0,9));
+// console.groupEnd();
+
+// console.groupEnd();
---
+++ js/test.js
@@ -0,0 +1,11 @@
+function hexHash(code){
+    let arr = []
+    for(let i = 0; i < code.length; i++){
+        console.log(code[i].charCodeAt(0).toString(16));
+        let numSum = 
+        arr.push[]
+    }
+
+  }
+
+  hexHash("Yo");
